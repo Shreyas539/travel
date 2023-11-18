3b6fb3de3537aa8d71f8907d0cc44065a7016fef
Shreyas539 <131728495+Shreyas539@users.noreply.github.com>
2023-11-18 09:39:43 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -67,6 +67,7 @@ const Footer = () => {
 
         <div className="border bg-gray-20" />
         <p className="regular-14 w-full text-center text-gray-30">2023 Hilink | All rights reserved</p>
+        <p className="regular-14 w-full text-center text-gray-30">Made with LOVE by Shreyas</p>
       </div>
     </footer>
   )
